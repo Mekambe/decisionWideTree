e70e873ec6f1d4339d4a3db8fa0e986b701454b9
Mekambe <kUbA1990>
2019-03-06 14:25:36 +0100

    basic security

--- src/main/java/com/decisionTree/wiki/configurations/SecurityConfig.java
+++ src/main/java/com/decisionTree/wiki/configurations/SecurityConfig.java
@@ -1,45 +1,61 @@
-//package com.decisionTree.wiki.configurations;
+package com.decisionTree.wiki.configurations;
 
 //import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+////import org.springframework.context.annotation.Configuration;
+////import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+////import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+////import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//
-//
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth)
-//            throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user").password("{noop}password").roles("USER")
-//                .and()
-//                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
-//    }
-//
-//    protected void configure(HttpSecurity http) throws Exception {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("kinguin").password("{noop}kinguinator").roles("USER");
+
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
 //        http.authorizeRequests()
-//                //.antMatchers("/", "/home").access("hasRole('USER')")
-//               // .antMatchers("/**").hasRole("ADMIN")
-//                .antMatchers("/h2-console/*").permitAll();//authorizeRequests();
-//                //.antMatchers("/h2-console").permitAll();
-////               http.authorizeRequests()
-////                .antMatchers("/h2-console").access("hasRole('USER')")
-////                       .and().httpBasic();
-//
-//        http.csrf().disable();
-//        http.headers().frameOptions().disable();
-//    }
+                //.antMatchers("/", "/home").access("hasRole('USER')")
+               // .antMatchers("/**").hasRole("ADMIN")
+//                .antMatchers("/**").permitAll();//authorizeRequests();
+                //.antMatchers("/h2-console").permitAll();
+               http.authorizeRequests()
+                .antMatchers("/**").access("hasRole('USER')")
+                .and().httpBasic();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
 
 
 
 
-//}
+
+
+
+
+}
 
 
