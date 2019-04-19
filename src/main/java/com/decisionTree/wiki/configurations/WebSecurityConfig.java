4d06599084e6ea42322fc511c0bd11de578e3080
Mekambe <kUbA1990>
2019-04-19 19:17:12 +0200

    Finished app

---
+++ src/main/java/com/decisionTree/wiki/configurations/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+/*
+ * Developed by Jan Misiorny
+ * February A.D. 2019
+// */
+//
+//package com.decisionTree.wiki.configurations;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.web.cors.CorsConfiguration;
+//
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity https) throws Exception {
+//        https.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+//    }
+//}
